fc3d6f3fddc3e3bb361066268974954ae61cc845
cristianbgp <cristian.granda.pastor@gmail.com>
2025-11-05 11:32:24 -0500

    Fix deploy

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -5,6 +5,8 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -12,5 +14,5 @@ export default defineConfig({
   },
 
   integrations: [react()],
-  output: "server",
+  adapter: cloudflare(),
 });
